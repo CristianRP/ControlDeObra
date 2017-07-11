3b7ed7f46c9c554d32c01a54242ca0617361c8c8
Cristian Ramirez <cristianramirezgt@gmail.com>
2017-07-11 17:09:21 -0600

    Butterknife and picasso dependencies added

--- app/src/main/java/com/depsa/controldeobra/ui/LoginActivity.java
+++ app/src/main/java/com/depsa/controldeobra/ui/LoginActivity.java
@@ -1,15 +1,35 @@
 package com.depsa.controldeobra.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.depsa.controldeobra.R;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.imgLogoLogin)
+    ImageView mImgLogo;
+    @BindView(R.id.txtUserName)
+    EditText mUserName;
+    @BindView(R.id.txtClave)
+    EditText mClave;
+    @BindView(R.id.btnEntrar)
+    Button mBtnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        Picasso.with(this)
+                .load(R.drawable.logo_depsa)
+                .into(mImgLogo);
     }
 }
